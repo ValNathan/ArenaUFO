815ad67ad9979ed3e3fe6d9b94d3b002203f7701
Nathan Valentin <nathan.valentin@laposte.net>
2021-03-01 12:23:09 +0100

    player generation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:ArenaUFO/engine/images.dart';
+import 'package:ArenaUFO/engine/sprite.dart';
 import 'package:ArenaUFO/engine/world_position.dart';
 import 'package:ArenaUFO/game_props.dart';
 import 'package:ArenaUFO/levels/levels.dart';
@@ -6,25 +8,35 @@ import 'package:ArenaUFO/models/create_model.dart';
 import 'package:ArenaUFO/models/game_model.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Images.instance.loadAll(['assets/images/sprites/player.png']);
+
+  final playerSprite = await Sprite.fromPath('assets/images/sprites/player.png',
+      width: GameProps.tileSize, height: GameProps.tileSize);
+  debugPrint('playerSprite: $playerSprite');
   final tilemap = Tilemap.build(Levels.simple);
   debugPrint('$tilemap');
   WorldPosition.tileSize = GameProps.tileSize;
   final gameModel = createModel(tilemap, GameProps.tileSize);
   debugPrint('$gameModel');
-  runApp(MyApp(gameModel: gameModel));
+  runApp(MyApp(gameModel: gameModel, playerSprite: playerSprite));
 }
 
 class MyApp extends StatelessWidget {
+  final Sprite playerSprite;
   final GameModel gameModel;
-  MyApp({this.gameModel});
+  MyApp({@required this.gameModel, @required this.playerSprite});
 
   Widget build(BuildContext context) {
     return Container(
       color: Colors.tealAccent,
       child: CustomPaint(
         size: Size(double.infinity, double.infinity),
-        painter: CanvasPainter(gameModel: gameModel),
+        painter: CanvasPainter(
+          gameModel: gameModel,
+          playerSprite: playerSprite,
+        ),
       ),
     );
   }
@@ -33,9 +45,12 @@ class MyApp extends StatelessWidget {
 class CanvasPainter extends CustomPainter {
   final Paint _circlePaint;
   final Paint _gridPaint;
+  final Sprite playerSprite;
   final GameModel gameModel;
-  CanvasPainter({@required this.gameModel})
-      : _circlePaint = Paint()
+  CanvasPainter({
+    @required this.gameModel,
+    @required this.playerSprite,
+  })  : _circlePaint = Paint()
           ..color = Colors.red
           ..style = PaintingStyle.fill,
         _gridPaint = Paint()
@@ -46,10 +61,16 @@ class CanvasPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     _lowerLeftCanvas(canvas, size.height);
     _drawGrid(canvas, size);
+    _drawPlayer(canvas);
+  }
+
+  void _drawPlayer(Canvas canvas) {
     final radius = 10.0;
     final playerTilePosition = gameModel.player.tilePosition;
     final center = WorldPosition.fromTilePosition(playerTilePosition);
-    canvas.drawCircle(center.toOffset(), radius, _circlePaint);
+    final sprite = Sprite('assets/images/sprites/player.png');
+    canvas.drawCircle(center.toOffset(), 5, _circlePaint);
+    playerSprite.render(canvas, center.toOffset());
   }
 
   void _lowerLeftCanvas(Canvas canvas, double height) {
